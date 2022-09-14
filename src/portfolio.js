5231303ea6d279d0c9dc70bccb237557c0169fa0
Niko <soder.n@gmail.com>
2022-09-14 13:16:18 +0300

    Style project cards

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,12 +4,12 @@ let li = document.querySelectorAll(".nav-li");
 
 // hide nav when user clicks nav button
 button.addEventListener("click", () => {
-    nav.classList.toggle("hide-nav");
-})
+  nav.classList.toggle("hide-nav");
+});
 
 // hide nav when user clicks list item
-li.forEach(liItem => {
-    liItem.addEventListener('click', () => {
-        nav.classList.toggle("hide-nav");
-    })
+li.forEach((liItem) => {
+  liItem.addEventListener("click", () => {
+    nav.classList.toggle("hide-nav");
+  });
 });
